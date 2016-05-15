18bc83416880c94766b776fb6f85ec4669f605c2
Robert President <bigmike73184@gmail.com>
2016-05-15 16:58:01 -0500

    update2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,7 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
-  end
-
-  def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -18,7 +15,41 @@ class ProductsController < ApplicationController
       image: params[:image],
       description: params[:description]
       )
+flash[:success] = "Product Created"
+redirect_to "/products/#{@product.id}"  end
 
-    render 'show.html.erb'
+  def show
+    @product = Product.find(params[:id])
   end
+
+  def edit
+   @product = Product.find_by(id: params[:id]) 
+  end
+
+  def update
+   @product = Product.find_by(id: params[:id]) 
+
+   @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+      )
+
+   flash[:success] = "Product Created"
+   render 'show.html.erb'
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+
+    flash[:warning] = "Product Went Kablooey!!!"
+    redirect_to "/"
+  end
+
 end
+
+  
+
+
